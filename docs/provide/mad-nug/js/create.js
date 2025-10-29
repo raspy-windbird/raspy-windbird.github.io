fa745574408ce10b971a28de385361cbc7741342
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-29 17:17:21 +0900

    Update create.js

--- docs/provide/mad-nug/js/create.js
+++ docs/provide/mad-nug/js/create.js
@@ -2,10 +2,12 @@ function create (ele,origin_id,attribute) {
     // ele: 作りたい要素のタグ「h1」など
     // origin_id: 親となる要素のid
     // attribute: 変更したい要素を「value,hellow」のようなペアで入れる
+    const parsed = JSON.parse(attribute);
+    
     const element = document.createElement(ele);
-
-    for (let key in attribute){
-        element.setAttribute(key,JSON.stringify(attribute));
+    
+    for (let key in parsed){
+        element.setAttribute(key,parsed);
     }
 
     document.getElementById(origin_id).appendChild(element);
