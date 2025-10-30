0576160c8f0852590ddfcd80471f06796f8b9079
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-31 00:20:28 +0900

    update

--- docs/provide/mad-nug/js/boot.js
+++ docs/provide/mad-nug/js/boot.js
@@ -3,11 +3,9 @@ import dom from "./assets/dom.js";
 async function boot(){
     const Dom = new dom();
     Dom.create("input","boot",{type:"button",value:"SYSTEM",id:"boot"});
+    
     const button = document.getElementById("boot");
-    
     await button_clicked(button);
-    
-    button.remove();
 }
 
 async function button_clicked (ele){
@@ -16,6 +14,7 @@ async function button_clicked (ele){
     if (ele) {
         ele.addEventListener("click", () => {
             console.log("/boot.js ---SYSTEM STARTED---");
+            button.remove();
         }, { once: true });
     }});
 }
