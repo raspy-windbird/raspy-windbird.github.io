d3291f8ed590eb9d34b3aad5da18d3cae0e7d081
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-30 12:13:45 +0900

    update

--- docs/provide/mad-nug/js/boot.js
+++ docs/provide/mad-nug/js/boot.js
@@ -1,5 +1,16 @@
 import create from "./create.js";
 
+//main//
 function boot(){
-    create("input","boot",{type:"button",value:"SYSTEM"});
+    boot_button();
 }
+
+//sub//
+function boot_button(){
+    let button = create("input","boot",{type:"button",value:"SYSTEM"});
+    
+    
+}
+
+//boot export
+export default boot;
