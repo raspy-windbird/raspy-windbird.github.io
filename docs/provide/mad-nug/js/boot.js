5ea7bb180d39730bfcd3b17603dd89fbcba1264a
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-30 19:01:30 +0900

    use async

--- docs/provide/mad-nug/js/boot.js
+++ docs/provide/mad-nug/js/boot.js
@@ -5,7 +5,21 @@ function boot(){
 }
 
 
-//sub//
+////sub////
+//boot_button//
+async function boot_button(){
+    await button_create();
+    await button_clicked();
+}
+
+async function button_create(){
+    create("input","boot",{type:"button",value:"SYSTEM",id:"boot_button"});
+}
+async function button_clicked (){
+    button.addEventListener("click",resolve);
+}
+
+/*
 function boot_button(){
     create("input","boot",{type:"button",value:"SYSTEM",id:"boot_button"});
     const button = document.getElementById("boot_button");
@@ -18,8 +32,8 @@ function boot_button(){
             button.remove();
         }
     )
-
 }
+*/
 
 //boot export
 export default boot;
