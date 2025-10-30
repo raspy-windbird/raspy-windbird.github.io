2b06df9e98b33bbc380d04ca8d7f87eb3af38e51
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-10-30 12:39:57 +0900

    update

--- docs/provide/mad-nug/js/boot.js
+++ docs/provide/mad-nug/js/boot.js
@@ -8,8 +8,10 @@ function boot(){
 //sub//
 function boot_button(){
     let button = create("input","boot",{type:"button",value:"SYSTEM"});
-    
-    
+    button.addEventListener("click",
+    function (){
+        console.log("abc");
+    })
 }
 
 //boot export
